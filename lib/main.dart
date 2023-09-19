334d6ced0a46c1746b53e919f1de554e8e0a8c69
Joan Fortes <joanfortes@hotmail.com>
2023-09-19 12:23:35 -0300

    adding login page and init project

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:mobile/modules/splashScreen/page/splash_screen.dart';
+
+import 'modules/login_page/page/login_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue[900]!),
+        useMaterial3: true,
+      ),
+      routes: {
+        LoginPage.pageName: (context) => const LoginPage(),
+      },
+      home: const SplashScreen(),
+    );
+  }
+}
